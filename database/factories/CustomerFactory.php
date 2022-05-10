86fe4dc916aab2de0065e3fd4674c61982951a2b
Jonab <jonabsf@gmail.com>
2022-05-10 05:15:40 -0300

    Update Factories

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -10,11 +10,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class CustomerFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Customer::class;
+    
     public function definition()
     {
         return [
--- database/factories/TableFactory.php
+++ database/factories/TableFactory.php
@@ -3,21 +3,19 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Table;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Table>
  */
 class TableFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Table::class;
+    
     public function definition()
     {
         return [
-            //
+            'number' => $this->faker->integer(),
         ];
     }
 }
